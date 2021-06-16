938417d2bb5451a246c6cefced8be832dc29160e
Liming Cai <daybreak.chua@gmail.com>
2021-06-16 11:40:53 -0700

    Update phyloherb.py

--- phyloherb.py
+++ phyloherb.py
@@ -191,8 +191,7 @@ def gene_extra(input_dir,suffix,output_dir):
 					output_handle=open(output_dir+'/'+feature.qualifiers['gene'][0]+'.gene.fas','a')
 					d=output_handle.write(">%s\n%s\n" % (feature.qualifiers['gene'][0]+'_'+f.split('.')[0],feature.extract(gb_recs).seq))
 					output_handle.close()
-				except KeyError:
-					print(feature.qualifiers)
+				except KeyError:pass
 
 def geneblock_extra(input_dir,suffix,output_dir,gene_def):
 	filenames=os.listdir(input_dir)
